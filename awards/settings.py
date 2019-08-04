bd9d0a653b382285aaad14c77e268d932bafac08
Machanga <florenceshiru765@gmail.com>
2019-08-04 11:30:08 +0300

    add database configurations to connect the database to the application

--- awards/settings.py
+++ awards/settings.py
@@ -86,10 +86,10 @@ if config('MODE')=="dev":
     DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'awards',
-        'USER': 'florence',
-        'PASSWORD': 'kambo',
-        'HOST': '127.0.0.1',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
         'PORT': '',
         }    
     }
