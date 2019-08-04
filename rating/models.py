52ee08465f6923b5cdc44b792567e6110682fe60
Machanga <florenceshiru765@gmail.com>
2019-08-04 12:19:01 +0300

    create the project model that will create the project table in the database where all the projects added to the application will be stored

--- rating/models.py
+++ rating/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    '''
+    project class for all the projects that will be added to the application
+    '''
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=1000)
+    link = models.URLField(max_length=200)
