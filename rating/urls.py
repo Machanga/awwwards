f4ea86446f657786a962beff50a15158fce2070d
Machanga <florenceshiru765@gmail.com>
2019-08-05 09:21:55 +0300

    add the url routes for all the view function that will help get the users route request

--- rating/urls.py
+++ rating/urls.py
@@ -1,15 +1,17 @@
-from django.conf.urls import url
-from django.conf.urls.static import static
-from django.conf import settings
+from django.conf.urls import url,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
-
-urlpatterns=[
-    url(r'^$',views.index,name = 'index'),
-    url(r'^search/', views.search_results, name='search_results'),
-    url(r'^new/project$', views.post_project, name='post_project'),
-    url(r'profile/user/(\d+)/$',views.profile,name='profile'),
+urlpatterns = [
+   url('^$',views.index,name = 'index'),
+    url(r'^post_project$',views.new_project,name='new_project'),
+    url(r'^search/$',views.search_results,name='search_results'),
+    url(r'^profile/(?P<username>\w{0,60})',views.profile,name='profile'),
+    url(r'^edit_profile/(?P<username>\w{0,50})',views.edit_profile,name='edit_profile'),
+    url(r'^project/(\d+)',views.project,name='project'),
+    url(r'^api/projects/$', views.ProjectList.as_view()),
+    url(r'^api/profiles/$', views.ProfileList.as_view()),
 ]
-
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
